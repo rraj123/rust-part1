66e97be62987b26150fb60f74a36115ad40897e6
Raj Ramalingam <rajramalingam@rajs-mbp.lan>
2024-11-28 11:58:07 -0800

    First pgm

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+
+#[derive(Debug)]
+struct Deck{
+    cards: Vec<String>
+}
+
+
+fn main() {
+    
+    let suits =["Hearts","Diamond","Spades"];
+    let values =["Ace","Two","Three"];
+
+    let mut cards = vec![];
+
+    for suit in suits{
+        for value in values {
+           let card = format!("{} of {}",value, suit);   
+           cards.push(card);  
+        }
+    }
+    
+    let deck = Deck{cards};
+    
+    println!("Heres your deck {:#?}", deck)
+
+}
